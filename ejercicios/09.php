c01e717c0ffa4c9ed196d6d45eada704d5232b1b
Ciprian Knor <knor.ciprian06@gmail.com>
2020-09-28 18:00:14 +0100

    ejercicio9

---
+++ ejercicios/09.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $array1 = ["Espania", "Rumania", "Dinamarca", "Japon"];
+    
+    echo $array1[0] . "<br>";
+    echo $array1[1] . "<br>";
+    echo $array1[2] . "<br>";
+    echo $array1[3] . "<br>";
+    echo $array1[4] . "<br>";
+
+    ?>
+</body>
+</html>
